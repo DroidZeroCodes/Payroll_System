a66f1b0cd56558d6c74bad8b77f0837a9ca520aa
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-01-18 23:59:09 +0800

    Added comments

--- src/OOP/HW4_Abstraction/SalesCalculator.java
+++ src/OOP/HW4_Abstraction/SalesCalculator.java
@@ -5,49 +5,53 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-interface Item {
-    double calculateSales();
-    String getItemDetails();
+interface Item { //interface for phone and repair services
+    double calculateSales(); //abstract method
+    String getItemDetails(); //abstract method
 }
 
-class Phone implements Item {
+class Phone implements Item { //class for phone, implements interface
 
-    private double price;
+
+    private double price; //instance variables to store price and quantity sold, declared as private for encapsulation
     private int quantitySold;
-    public Phone(double price, int quantitySold) {
+
+
+    public Phone(double price, int quantitySold) { //constructor to initialize price and quantity sold
         this.price = price;
         this.quantitySold = quantitySold;
     }
 
-    public double calculateSales() {
+    public double calculateSales() { //override abstract method to calculate sales
         return price * quantitySold;
     }
-
-    public String getItemDetails() {
+    public String getItemDetails() { //override abstract method to get item details
         return "Phone: Price=" + price + ", Quantity Sold=" + quantitySold;
     }
 }
 
-class RepairServices implements Item {
+class RepairServices implements Item { //class for repair services, implements interface
 
-    private double pricePerHour;
+    private double pricePerHour; //instance variables to store price per hour and number of hours
     private int numberOfHours;
-    public RepairServices(double pricePerHour, int numberOfHours) {
+    public RepairServices(double pricePerHour, int numberOfHours) { //constructor to initialize price per hour and number of hours
         this.pricePerHour = pricePerHour;
         this.numberOfHours = numberOfHours;
     }
 
-    public double calculateSales() {
+    public double calculateSales() { //override abstract method to calculate sales
         return pricePerHour * numberOfHours;
     }
 
-    public String getItemDetails() {
+    public String getItemDetails() { //override abstract method to get item details
         return "Repair Services: Price Per Hour=" + pricePerHour + ", Number of Hours=" + numberOfHours;
     }
 }
 
 
-public class SalesCalculator extends JFrame implements ActionListener {
+public class SalesCalculator extends JFrame implements ActionListener { //class for sales calculator with GUI
+
+    //GUI components
     private JButton calculateButton;
     private JLabel phoneLabel;
     private JLabel repairServicesLabel;
@@ -61,57 +65,63 @@ public class SalesCalculator extends JFrame implements ActionListener {
     private JTextField txt_NumberOfHours;
     private JTextArea resultArea;
 
-    public SalesCalculator() {
-        setTitle("Electronics Store Sales Calculator");
-        setSize(550, 450);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(6,1));
+    public SalesCalculator() { //constructor to initialize GUI
+        setTitle("Electronics Store Sales Calculator");  //set title of frame
+        setSize(550, 450);                 // set size of frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //exit on close
+        setLayout(new GridLayout(6,1));      // set layout of frame
 
-        //Phone
+        //Phone label
         phoneLabel = new JLabel("Phone");
         phoneLabel.setFont(new Font("Arial", Font.BOLD, 20));
 
+        //Phone panel
         JPanel phonePanel = new JPanel();
         phonePanel.setLayout(new FlowLayout());
 
-        phonePanel.add(lbl_PhonePrice = new JLabel("Price"));
-        phonePanel.add(txt_Price = new JTextField(10));
+        //add components to phone panel
+        phonePanel.add(lbl_PhonePrice = new JLabel("Price"));  //price label
+        phonePanel.add(txt_Price = new JTextField(10));     //price text field to enter price
 
-        phonePanel.add(lbl_PhoneQuantitySold = new JLabel("Quantity Sold"));
-        phonePanel.add(txt_QuantitySold = new JTextField(10));
+        phonePanel.add(lbl_PhoneQuantitySold = new JLabel("Quantity Sold")); //quantity sold label
+        phonePanel.add(txt_QuantitySold = new JTextField(10));            //quantity sold text field to enter quantity
 
-        //Repair
+        //Repair Services label
         repairServicesLabel = new JLabel("Repair Services");
         repairServicesLabel.setFont(new Font("Arial", Font.BOLD, 20));
 
+        //Repair panel
         JPanel repairPanel = new JPanel();
         repairPanel.setLayout(new FlowLayout());
 
-        repairPanel.add(lbl_RepairPricePerHour = new JLabel("Price Per Hour"));
-        repairPanel.add(txt_PricePerHour = new JTextField(10));
+        //add components to repair panel
+        repairPanel.add(lbl_RepairPricePerHour = new JLabel("Price Per Hour")); //price per hour label
+        repairPanel.add(txt_PricePerHour = new JTextField(10));              //price per hour text field to enter price per hour
 
-        repairPanel.add(lbl_RepairNumberOfHours = new JLabel("Number of Hours"));
-        repairPanel.add(txt_NumberOfHours = new JTextField(10));
+        repairPanel.add(lbl_RepairNumberOfHours = new JLabel("Number of Hours")); //number of hours label
+        repairPanel.add(txt_NumberOfHours = new JTextField(10));               //number of hours text field to enter number of hours
 
         //Buttons
-        calculateButton = new JButton("Calculate");
-        calculateButton.addActionListener(this);
+        calculateButton = new JButton("Calculate"); //calculate button
+        calculateButton.addActionListener(this);      //add action listener to know when calculate button is clicked
 
         //Result
-        resultArea = new JTextArea();
+        resultArea = new JTextArea();   //text area to display sales result
 
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(calculateButton);
+        JPanel buttonPanel = new JPanel(); //panel for buttons
+        buttonPanel.add(calculateButton);  //add calculate button to panel
 
-        add(phoneLabel);
-        add(phonePanel);
-        add(repairServicesLabel);
-        add(repairPanel);
-        add(buttonPanel);
-        add(resultArea);
+        //add components to frame
+        add(phoneLabel);          //start with phone label
+        add(phonePanel);          //add phone panel
+        add(repairServicesLabel); //add repair services label
+        add(repairPanel);         //add repair panel
+        add(buttonPanel);         //add button panel
+        add(resultArea);          //add result area
     }
 
+    //Getter Methods
     private String getPrice() {
         return txt_Price.getText();
     }
@@ -128,31 +138,36 @@ public class SalesCalculator extends JFrame implements ActionListener {
         return txt_NumberOfHours.getText();
     }
 
+    //Action Listener for calculate button
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == calculateButton) {
 
+            //parsing (converting) text fields (what the user enters) to double and integer
             double price = Double.parseDouble(getPrice());
             double pricePerHour = Double.parseDouble(getPricePerHour());
             int quantitySold = Integer.parseInt(getQuantitySold());
             int numberOfHours = Integer.parseInt(getNumberOfHours());
 
-            Item phone = new Phone(price, quantitySold);
-            Item repairServices = new RepairServices(pricePerHour, numberOfHours);
+            //create phone and repair services objects
+            Item phone = new Phone(price, quantitySold);                           //passing price and quantity sold to constructor
+            Item repairServices = new RepairServices(pricePerHour, numberOfHours); //passing price per hour and number of hours to constructor
 
+            //running calculate sales and getting item details
             double phoneSales = phone.calculateSales();
             double repairSales = repairServices.calculateSales();
             String phoneDetails = phone.getItemDetails();
             String repairDetails = repairServices.getItemDetails();
 
+            //displaying item details and sales in result area, "\n" for new line
             resultArea.setText("Phone Details: " + phoneDetails + "\nRepair Services Details: " + repairDetails + "\nPhone Sales: " + phoneSales + "\nRepair Services Sales: " + repairSales);
         }
     }
 
-
+    //main method
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            SalesCalculator electronicsStore = new SalesCalculator();
-            electronicsStore.setVisible(true);
+        SwingUtilities.invokeLater(() -> { //run on swing thread
+            SalesCalculator electronicsStore = new SalesCalculator(); //create object
+            electronicsStore.setVisible(true); //make frame visible
         });
     }
 }
