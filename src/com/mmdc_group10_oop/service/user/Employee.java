600f731bc739c638633000a2ef1b7564f790b859
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-02-21 11:58:48 +0800

    modified to current UI

--- src/com/mmdc_group10_oop/service/user/Employee.java
+++ src/com/mmdc_group10_oop/service/user/Employee.java
@@ -1,7 +1,11 @@
 package com.mmdc_group10_oop.service.user;
 
 import com.mmdc_group10_oop.DataHandlingModule.*;
-import com.mmdc_group10_oop.ui.EmployeeUI;
+import com.mmdc_group10_oop.service.actions.interfaces.AttendanceManagement;
+import com.mmdc_group10_oop.service.actions.interfaces.LeaveManagement;
+import com.mmdc_group10_oop.service.actions.interfaces.PayslipManagement;
+import com.mmdc_group10_oop.service.actions.interfaces.ProfileManagement;
+import com.mmdc_group10_oop.ui.*;
 import com.opencsv.exceptions.CsvException;
 
 import javax.swing.*;
@@ -12,57 +16,92 @@ public class Employee implements ProfileManagement, AttendanceManagement, LeaveM
     int employeeID;
     String firstName;
     String lastName;
-    private PayrollInformation payrollInfo;
-    private EmployeeProfile personalInfo;
-    private EmploymentInformation employmentInfo;
-    private List<LeaveRecord> leaveRequests;
-    private List<AttendanceRecord> attendanceRecords;
-    private Payslip payslip;
-    private LeaveBalance leaveBalance;
-    public Employee(int employeeID) throws IOException, CsvException {
+    protected PayrollInformation payrollInfo;
+    protected EmployeeProfile personalInfo;
+    protected EmploymentInformation employmentInfo;
+    protected List<LeaveRecord> leaveRequests;
+    protected List<AttendanceRecord> attendanceRecords;
+    protected Payslip payslip;
+    protected LeaveBalance leaveBalance;
+    protected final EmployeeUI ui;
+
+    protected final myProfilePanel profilePanel;
+    protected final attendancePanel attendancePanel;
+
+    protected final myPayslipPanel payslipPanel;
+    protected final leavePanel leavePanel;
+    public Employee(int employeeID, EmployeeUI ui) throws IOException, CsvException {
         this.employeeID = employeeID;
-        System.out.println("Employee ID: " + employeeID);
+        this.ui = ui;
 
         personalInfo = new EmployeeProfile(employeeID);
         employmentInfo = new EmploymentInformation(employeeID);
         payrollInfo = new PayrollInformation(employeeID);
+
+
+        profilePanel = ui.empProfilePanel;
+        attendancePanel = ui.empAttendancePanel;
+        payslipPanel = ui.empPayslipPanel;
+        leavePanel = ui.empLeavePanel;
+
+
     }
 
-    @Override
-    public void viewProfile(EmployeeUI employeeUI) {
+    public PayrollInformation payrollInfo() {
+        return payrollInfo;
+    }
+
+    public void setPayrollInfo(PayrollInformation payrollInfo) {
+        this.payrollInfo = payrollInfo;
+    }
+
+    public EmployeeProfile personalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(EmployeeProfile personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    public EmploymentInformation employmentInfo() {
+        return employmentInfo;
+    }
+
+    public void setEmploymentInfo(EmploymentInformation employmentInfo) {
+        this.employmentInfo = employmentInfo;
+    }
+
+
+    public void displayProfile() {
         //Employee Profile
-        employeeUI.nameTxtField.setText(personalInfo.firstName() + " " + personalInfo.lastName());
-        employeeUI.birthdayTxtField.setText(personalInfo.dob());
-        employeeUI.phoneNoTxtField.setText(personalInfo.phoneNum());
-        employeeUI.addressTxtArea.setText(personalInfo.address());
+        profilePanel.nameTxtField.setText(personalInfo().firstName() + " " + personalInfo().lastName());
+        profilePanel.birthdayTxtField.setText(personalInfo().dob());
+        profilePanel.phoneNoTxtField.setText(personalInfo().phoneNum());
+        profilePanel.addressTxtArea.setText(personalInfo().address());
 
         //Employment
-        employeeUI.empIDTxtField.setText(String.valueOf(employeeID));
-        employeeUI.departmentTxtField.setText(employmentInfo.department());
-        employeeUI.positionTxtField.setText(employmentInfo.position());
-        employeeUI.supervisoTxtField.setText(String.valueOf(employmentInfo.supervisor()));
-        employeeUI.statusTxtField.setText(employmentInfo.status());
+        profilePanel.empIDTxtField.setText(String.valueOf(employmentInfo().employeeID()));
+        profilePanel.departmentTxtField.setText(employmentInfo().department());
+        profilePanel.positionTxtField.setText(employmentInfo().position());
+        profilePanel.supervisoTxtField.setText(String.valueOf(employmentInfo().supervisor()));
+        profilePanel.statusTxtField.setText(employmentInfo().status());
 
         //Payroll
-        employeeUI.basicSalaryTxtField.setText(String.valueOf(payrollInfo.basicSalary()));
-        employeeUI.hourlyRateTxtField.setText(String.valueOf(payrollInfo.hourlyRate()));
-        employeeUI.semiMonthlyTxtField.setText(String.valueOf(payrollInfo.semiMonthlyRate()));
+        profilePanel.basicSalaryTxtField.setText(String.valueOf(payrollInfo().basicSalary()));
+        profilePanel.hourlyRateTxtField.setText(String.valueOf(payrollInfo().hourlyRate()));
+        profilePanel.semiMonthlyTxtField.setText(String.valueOf(payrollInfo().semiMonthlyRate()));
 
         //Allowances
-        employeeUI.riceSubsidyTxtField.setText(String.valueOf(payrollInfo.riceSubsidy()));
-        employeeUI.phoneAllowanceTxtField.setText(String.valueOf(payrollInfo.phoneAllowance()));
-        employeeUI.clothingAllowanceTxtField.setText(String.valueOf(payrollInfo.clothingAllowance()));
+        profilePanel.riceSubsidyTxtField.setText(String.valueOf(payrollInfo().riceSubsidy()));
+        profilePanel.phoneAllowanceTxtField.setText(String.valueOf(payrollInfo().phoneAllowance()));
+        profilePanel.clothingAllowanceTxtField.setText(String.valueOf(payrollInfo().clothingAllowance()));
 
         //Deductions
-        employeeUI.sssNoTextField.setText(String.valueOf(payrollInfo.sssNo()));
-        employeeUI.PhilhealthNoTxtField.setText(String.valueOf(payrollInfo.philHealthNo()));
-        employeeUI.pagibigNoTxtField.setText(String.valueOf(payrollInfo.pagIbigNo()));
-        employeeUI.tinNoTxtField.setText(String.valueOf(payrollInfo.tinNo()));
-    }
-
-    @Override
-    public void changePersonalInfo() {
-
+        profilePanel.sssNoTextField.setText(String.valueOf(payrollInfo().sssNo()));
+        profilePanel.philhealthNoTxtField.setText(String.valueOf(payrollInfo().philHealthNo()));
+        profilePanel.pagibigNoTxtField.setText(String.valueOf(payrollInfo().pagIbigNo()));
+        profilePanel.tinNoTxtField.setText(String.valueOf(payrollInfo().tinNo()));
+        // Set other UI components using model data
     }
 
     @Override
@@ -74,39 +113,29 @@ public class Employee implements ProfileManagement, AttendanceManagement, LeaveM
     public void clockOut() {
 
     }
+
     @Override
-    public void viewAttendanceRecord(JTable table) {
-//        DefaultTableModel model = (DefaultTableModel) table.getModel();
-//        model.setRowCount(0); // Clear existing rows
-//
-//        // Populate table with profile records
-//        for (AttendanceRecord record : employee.getAttendanceRecords()) {
-//            Object[] rowData = {
-//                    record.date().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
-//                    record.timeIn().format(DateTimeFormatter.ofPattern("HH:mm")),
-//                    record.timeOut().format(DateTimeFormatter.ofPattern("HH:mm")),
-//                    record.overTime(),
-//                    record.totalHours()
-//            };
-//            model.addRow(rowData);
-//        }
-    }
-    @Override
-    public void viewLeaveBalance() {
+    public void displayAttendanceRecord(JTable table) {
 
     }
+
+    @Override
+    public void displayLeaveBalance() {
+
+    }
+
     @Override
     public void submitLeaveRequest() {
 
     }
 
     @Override
-    public void viewLeaveStatus() {
+    public void displayLeaveStatus() {
 
     }
 
     @Override
-    public void viewPayslip() {
+    public void displayPayslip() {
 
     }
 }
