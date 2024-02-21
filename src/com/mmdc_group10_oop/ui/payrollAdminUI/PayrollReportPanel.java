6a0cc2f7a0340ae10d17744085ce9ec5641909bb
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-02-21 15:02:37 +0800

    changed package address

--- src/com/mmdc_group10_oop/ui/payrollAdminUI/PayrollReportPanel.java
+++ src/com/mmdc_group10_oop/ui/payrollAdminUI/PayrollReportPanel.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package com.mmdc_group10_oop.ui.payrollAdminUI;
+package com.mmdc_group10_oop.ui.PayrollAdminUI;
 
 /**
  *
