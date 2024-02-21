595c4470cb3828c1a083828a5eac9ddc018e4bb9
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-02-21 15:04:15 +0800

    added MyProfilePanel as an argument

--- src/com/mmdc_group10_oop/service/actions/interfaces/ProfileManagement.java
+++ src/com/mmdc_group10_oop/service/actions/interfaces/ProfileManagement.java
@@ -1,6 +1,8 @@
 package com.mmdc_group10_oop.service.actions.interfaces;
 
+import com.mmdc_group10_oop.ui.employee.MyProfilePanel;
+
 public interface ProfileManagement {
-    void displayProfile();
+    void displayProfile(MyProfilePanel panel);
 }
 
