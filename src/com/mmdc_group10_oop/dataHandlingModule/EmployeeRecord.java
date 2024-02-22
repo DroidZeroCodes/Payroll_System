93443447f0b61381346b03b36509d7db4a651469
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-02-23 03:25:10 +0800

    transferred employment information and payroll information into employee record to only use one class to avoid redundancy

--- src/com/mmdc_group10_oop/dataHandlingModule/EmployeeRecord.java
+++ src/com/mmdc_group10_oop/dataHandlingModule/EmployeeRecord.java
@@ -3,26 +3,73 @@ package com.mmdc_group10_oop.dataHandlingModule;
 import com.mmdc_group10_oop.dataHandlingModule.util.DataHandler;
 import com.mmdc_group10_oop.dataHandlingModule.util.Record;
 import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.List;
 
-public class PayrollInformation extends Record {
-    int employeeID;
-    String sssNo, philHealthNo, pagIbigNo, tinNo;
+public class EmployeeRecord extends Record {
+    private int employeeID;
+    private String firstName, lastName, dob, address, phoneNum;
+    private String sssNo, philHealthNo, pagIbigNo, tinNo;
     private Double riceSubsidy, phoneAllowance, clothingAllowance;
     private Double basicSalary, semiMonthlyRate, hourlyRate;
-    public PayrollInformation(int employeeID) throws CsvValidationException, IOException {
+    private String position;
+    private String department;
+    private String supervisor;
+    private String status;
+
+    public EmployeeRecord(int employeeID) throws CsvException, IOException {
         this.employeeID = employeeID;
         retrieveRecord();
     }
+
     public int employeeID() {
         return employeeID;
     }
 
     public void setEmployeeID(int employeeID) {
         this.employeeID = employeeID;
+    }
+
+    public String firstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String lastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String dob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String address() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String phoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
     }
 
     public String sssNo() {
@@ -47,6 +94,38 @@ public class PayrollInformation extends Record {
 
     public void setPagIbigNo(String pagIbigNo) {
         this.pagIbigNo = pagIbigNo;
+    }
+
+    public String position() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String department() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String supervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(String supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public String status() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String tinNo() {
@@ -105,8 +184,9 @@ public class PayrollInformation extends Record {
         this.hourlyRate = hourlyRate;
     }
 
+
     @Override
-    protected void retrieveRecord() throws CsvValidationException, IOException {
+    protected void retrieveRecord() throws CsvException, IOException {
         if (isValidKey(employeeID) && doesExist(primaryKey(),String.valueOf(employeeID))) {
             try {
                 DataHandler dataHandler = new DataHandler(filePath());
@@ -120,18 +200,29 @@ public class PayrollInformation extends Record {
                 } else {
                     String[] row = csv.get(0);
 
+                    DecimalFormat format = new DecimalFormat("#,##0.0#");
+                    format.setParseBigDecimal(true);
+
                     setEmployeeID(Integer.parseInt(row[0]));
+                    setLastName(row[1]);
+                    setFirstName(row[2]);
+                    setDob(row[3]);
+                    setAddress(row[4]);
+                    setPhoneNum(row[5]);
                     setSssNo(row[6]);
                     setPhilHealthNo(row[7]);
                     setTinNo(row[8]);
                     setPagIbigNo(row[9]);
+                    setStatus(row[10]);
+                    setDepartment(row[11]);
+                    setPosition(row[12]);
+                    setSupervisor(row[13]);
                     setBasicSalary(parseDoubleValue(row[14]));
                     setRiceSubsidy(parseDoubleValue(row[15]));
                     setPhoneAllowance(parseDoubleValue(row[16]));
                     setClothingAllowance(parseDoubleValue(row[17]));
                     setSemiMonthlyRate(parseDoubleValue(row[18]));
                     setHourlyRate(parseDoubleValue(row[19]));
-
                 }
             } catch (IOException | CsvException | NumberFormatException e ) {
                 throw new RuntimeException(e);
@@ -141,23 +232,19 @@ public class PayrollInformation extends Record {
 
     @Override
     public String toString() {
-        return "PayrollInformation{" +
-                "employeeID=" + employeeID +
-                ", sssNo='" + sssNo + '\'' +
-                ", philHealthNo='" + philHealthNo + '\'' +
-                ", pagIbigNo='" + pagIbigNo + '\'' +
-                ", tinNo='" + tinNo + '\'' +
-                ", riceSubsidy=" + riceSubsidy +
-                ", phoneAllowance=" + phoneAllowance +
-                ", clothingAllowance=" + clothingAllowance +
-                ", basicSalary=" + basicSalary +
-                ", semiMonthlyRate=" + semiMonthlyRate +
-                ", hourlyRate=" + hourlyRate +
+        return "employeeUI Profile{" +
+                "employeeID=" + employeeID + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dob='" + dob + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
                 '}';
     }
 
-    public static void main(String[] args) throws CsvValidationException, IOException {
-        PayrollInformation payroll = new PayrollInformation(1);
-        System.out.println(payroll);
+    public static void main(String[] args) throws IOException, CsvException {
+        EmployeeRecord employeeList = new EmployeeRecord(1);
+
+        System.out.println(employeeList);
     }
 }
