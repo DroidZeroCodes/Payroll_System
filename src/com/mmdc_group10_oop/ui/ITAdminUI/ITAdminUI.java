81669e37ff1f961f0c1cfceba87991e41276503f
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-02-26 13:56:11 +0800

    fixed errors

--- src/com/mmdc_group10_oop/ui/ITAdminUI/ITAdminUI.java
+++ src/com/mmdc_group10_oop/ui/ITAdminUI/ITAdminUI.java
@@ -1,9 +1,11 @@
 package com.mmdc_group10_oop.ui.ITAdminUI;
 
-import com.mmdc_group10_oop.ui.employeeUI.*;
-import com.mmdc_group10_oop.service.user.Employee;
+import com.mmdc_group10_oop.service.user.ITAdmin;
+import com.mmdc_group10_oop.ui.employeeUI.AttendancePanel;
+import com.mmdc_group10_oop.ui.employeeUI.LeavePanel;
+import com.mmdc_group10_oop.ui.employeeUI.MyPayslipPanel;
+import com.mmdc_group10_oop.ui.employeeUI.MyProfilePanel;
 import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -14,10 +16,10 @@ public class ITAdminUI extends javax.swing.JFrame {
     public MyPayslipPanel empPayslipPanel;
     public LeavePanel empLeavePanel;
 
-    protected Employee employee;
+    protected ITAdmin itAdmin;
 
-    
-    public ITAdminUI(int employeeID) throws IOException, CsvException {
+
+    public ITAdminUI(int itAdminID) throws IOException, CsvException {
         initComponents();
         setLocationRelativeTo(null);
         setResizable(false);
@@ -26,13 +28,13 @@ public class ITAdminUI extends javax.swing.JFrame {
         initializePanels();
         actions();
 
-        employee = new Employee(employeeID, this);
-        employee.displayProfile();
-        employee.displayAttendanceRecord();
+        itAdmin = new ITAdmin(itAdminID, this);
+        itAdmin.displayProfile();
+        itAdmin.displayAttendanceRecord();
     }
 
     // This method initializes the panels
-        private void initializePanels() {
+    private void initializePanels() {
         empProfilePanel = new MyProfilePanel();
         empAttendancePanel = new AttendancePanel();
         empPayslipPanel = new MyPayslipPanel();
@@ -43,15 +45,15 @@ public class ITAdminUI extends javax.swing.JFrame {
         mainPanel.add(empPayslipPanel, "payslip");
         mainPanel.add(empLeavePanel, "leave");
     }
-        
-        
-        //Method to set panels visible
-        private void resetPanelVisibility() {
+
+
+    //Method to set panels visible
+    private void resetPanelVisibility() {
         empProfilePanel.setVisible(false);
         empAttendancePanel.setVisible(false);
         empPayslipPanel.setVisible(false);
         empLeavePanel.setVisible(false);
-        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -67,7 +69,7 @@ public class ITAdminUI extends javax.swing.JFrame {
         mainPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("MotorPH Employee Portal");
+        setTitle("MotorPH itAdmin Portal");
         setMinimumSize(new java.awt.Dimension(1000, 700));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -78,44 +80,19 @@ public class ITAdminUI extends javax.swing.JFrame {
 
         myProfileBTN.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
         myProfileBTN.setText("My Profile");
-        myProfileBTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                myProfileBTNActionPerformed(evt);
-            }
-        });
 
         attedanceBTN.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
         attedanceBTN.setText("Attendance");
-        attedanceBTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                attedanceBTNActionPerformed(evt);
-            }
-        });
 
         payslipBTN.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
         payslipBTN.setText("Payslip");
-        payslipBTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                payslipBTNActionPerformed(evt);
-            }
-        });
 
         leaveBTN.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
         leaveBTN.setText("Leave");
-        leaveBTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                leaveBTNActionPerformed(evt);
-            }
-        });
 
         logoutBtn.setFont(new java.awt.Font("Segoe UI Semibold", 0, 12)); // NOI18N
         logoutBtn.setText("Logout");
         logoutBtn.setActionCommand("");
-        logoutBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                logoutBtnActionPerformed(evt);
-            }
-        });
 
         motorPHmainLabel.setFont(new java.awt.Font("Magneto", 0, 20)); // NOI18N
         motorPHmainLabel.setText("MotorPH");
@@ -126,44 +103,44 @@ public class ITAdminUI extends javax.swing.JFrame {
         javax.swing.GroupLayout sidePanelLayout = new javax.swing.GroupLayout(sidePanel);
         sidePanel.setLayout(sidePanelLayout);
         sidePanelLayout.setHorizontalGroup(
-            sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(sidePanelLayout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addGroup(sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                        .addComponent(myProfileBTN)
-                        .addComponent(attedanceBTN)
-                        .addComponent(payslipBTN)
-                        .addComponent(leaveBTN)
-                        .addComponent(mngUserBTN)
-                        .addComponent(logoutBtn))
-                    .addComponent(motorPHmainLabel))
-                .addContainerGap(36, Short.MAX_VALUE))
+                sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(sidePanelLayout.createSequentialGroup()
+                                .addGap(36, 36, 36)
+                                .addGroup(sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                                                .addComponent(myProfileBTN)
+                                                .addComponent(attedanceBTN)
+                                                .addComponent(payslipBTN)
+                                                .addComponent(leaveBTN)
+                                                .addComponent(mngUserBTN)
+                                                .addComponent(logoutBtn))
+                                        .addComponent(motorPHmainLabel))
+                                .addContainerGap(36, Short.MAX_VALUE))
         );
 
-        sidePanelLayout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {attedanceBTN, leaveBTN, logoutBtn, mngUserBTN, myProfileBTN, payslipBTN});
+        sidePanelLayout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[]{attedanceBTN, leaveBTN, logoutBtn, mngUserBTN, myProfileBTN, payslipBTN});
 
         sidePanelLayout.setVerticalGroup(
-            sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, sidePanelLayout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(motorPHmainLabel)
-                .addGap(141, 141, 141)
-                .addComponent(myProfileBTN)
-                .addGap(18, 18, 18)
-                .addComponent(attedanceBTN)
-                .addGap(18, 18, 18)
-                .addComponent(payslipBTN)
-                .addGap(18, 18, 18)
-                .addComponent(leaveBTN)
-                .addGap(18, 18, 18)
-                .addComponent(mngUserBTN)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 225, Short.MAX_VALUE)
-                .addComponent(logoutBtn)
-                .addGap(68, 68, 68))
+                sidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, sidePanelLayout.createSequentialGroup()
+                                .addGap(31, 31, 31)
+                                .addComponent(motorPHmainLabel)
+                                .addGap(141, 141, 141)
+                                .addComponent(myProfileBTN)
+                                .addGap(18, 18, 18)
+                                .addComponent(attedanceBTN)
+                                .addGap(18, 18, 18)
+                                .addComponent(payslipBTN)
+                                .addGap(18, 18, 18)
+                                .addComponent(leaveBTN)
+                                .addGap(18, 18, 18)
+                                .addComponent(mngUserBTN)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 225, Short.MAX_VALUE)
+                                .addComponent(logoutBtn)
+                                .addGap(68, 68, 68))
         );
 
-        sidePanelLayout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {attedanceBTN, leaveBTN, logoutBtn, mngUserBTN, myProfileBTN, payslipBTN});
+        sidePanelLayout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[]{attedanceBTN, leaveBTN, logoutBtn, mngUserBTN, myProfileBTN, payslipBTN});
 
         getContentPane().add(sidePanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 180, 700));
 
@@ -175,7 +152,7 @@ public class ITAdminUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void actions(){
+    private void actions() {
         myProfileBTN.addActionListener(e -> {
             resetPanelVisibility();
             empProfilePanel.setVisible(true);
@@ -200,16 +177,12 @@ public class ITAdminUI extends javax.swing.JFrame {
             dispose();
         });
 
-        empAttendancePanel.clockInBTN.addActionListener(e -> {
-            try {
-                employee.clockIn();
-            } catch (CsvValidationException | IOException ex) {
-                throw new RuntimeException(ex);
-            }
+        empAttendancePanel.clockInBTN().addActionListener(e -> {
+            itAdmin.clockIn();
         });
 
-        empAttendancePanel.clockOutBTN.addActionListener(e -> {
-            employee.clockOut();
+        empAttendancePanel.clockOutBTN().addActionListener(e -> {
+            itAdmin.clockOut();
         });
     }
 
@@ -224,7 +197,7 @@ public class ITAdminUI extends javax.swing.JFrame {
             frame.setVisible(true);
         });
     }
-    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton attedanceBTN;
     private javax.swing.JButton leaveBTN;
